438b754e658beda4f2bf7073c0b036cb71f60113
agdholo <agdholo@gmail.com>
2021-01-11 01:55:02 +0800

    wip(service): add qrcode

---
+++ app/Service/QrCodeService.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Service;
+
+use Endroid\QrCode\QrCode;
+
+class QrCodeService
+{
+
+	/**
+	 * @var QrCode
+	 */
+	private QrCode $qrcode;
+
+	/**
+	 * @param string $data
+	 *
+	 * @return $this
+	 */
+	public function generate(string $data): static
+	{
+		$this->qrcode = new QrCode($data);
+		return $this;
+	}
+
+	/**
+	 * @param int $size
+	 *
+	 * @return $this
+	 */
+	public function size(int $size): static
+	{
+		$this->qrcode->setSize($size);
+		return $this;
+	}
+
+	/**
+	 * @param string $format
+	 *
+	 * @return $this
+	 */
+	public function format(string $format): static
+	{
+		$this->qrcode->setWriterByName($format);
+		return $this;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getByImage(): string
+	{
+		return $this->qrcode->writeString();
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getByUrl(): string
+	{
+		return $this->qrcode->writeDataUri();
+	}
+}
