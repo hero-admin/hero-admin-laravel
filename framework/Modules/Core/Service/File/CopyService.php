8255f1e2b52374da7c030ab97f94e3c7dbdd6c40
agdholo <agdholo@gmail.com>
2021-02-23 21:15:51 +0800

    feat(service): add copy file service

---
+++ framework/Modules/Core/Service/File/CopyService.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Framework\Modules\Core\Service\File;
+
+class CopyService
+{
+	/**
+	 * @var string
+	 */
+	private string $from;
+
+	/**
+	 * @var string
+	 */
+	private string $to;
+
+	/**
+	 * @param string $from
+	 *
+	 * @return CopyService
+	 */
+	public function from(string $from): static
+	{
+		$this->from = $from;
+		return $this;
+	}
+
+	/**
+	 * @param string $to
+	 *
+	 * @return CopyService
+	 */
+	public function to(string $to): static
+	{
+		$this->to = $to;
+		return $this;
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function copy(): bool
+	{
+		return copy($this->from, $this->to);
+	}
+}
